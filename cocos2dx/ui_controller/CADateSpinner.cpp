29a0d9857bd7a29959193c4248b937b1b715f114
juguanhui <juguanhui@xiaomi.com>
2014-06-04 23:40:32 +0800

    添加CADateSpinner控件

---
+++ cocos2dx/ui_controller/CADateSpinner.cpp
@@ -0,0 +1,9 @@
+//
+//  CADateSpinner.cpp
+//  cocos2dx
+//
+//  Created by juguanhui on 14-6-4.
+//  Copyright (c) 2014年 cocos2d-x. All rights reserved.
+//
+
+#include "CADateSpinner.h"
---
+++ cocos2dx/ui_controller/CADateSpinner.h
@@ -0,0 +1,14 @@
+//
+//  CADateSpinner.h
+//  cocos2dx
+//
+//  Created by juguanhui on 14-6-4.
+//  Copyright (c) 2014年 cocos2d-x. All rights reserved.
+//
+
+#ifndef cocos2dx_CADateSpinner_h
+#define cocos2dx_CADateSpinner_h
+
+
+
+#endif
