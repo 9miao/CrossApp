bd91c020a0535953e93356e433de6485c7559db0
马国阳 <q2590@163.com>
2014-10-27 17:59:02 +0800

    添加文字风格配置读取接口

--- CrossApp/view/CALabelStyle.h
+++ CrossApp/view/CALabelStyle.h
@@ -2,23 +2,55 @@
 #define CALabelStyle_h__
 
 #include "CAView.h"
+#include <map>
 using std::string;
+using std::map;
 
 NS_CC_BEGIN
 
-class CALabelStyle : public CAObject
+class CC_DLL CALabelStyle : public CAObject
 {
 public:
 	CALabelStyle();
 
-public:
-	CC_SYNTHESIZE_IS_PASS_BY_REF(string, m_sFontName, FontName);
+	static const CALabelStyle* getStyle(const string& sStyleName);
+
+	int getFontHeight();
+
+	int getStringWidth( const std::string& text);
+
+	int cutStringByWidth(const std::string& text, int iLimitWidth, int& cutWidth);
+
+	int getStringHeight(const std::string& text, int iLimitWidth);
+
+protected:
+	CC_SYNTHESIZE_PASS_BY_REF(string, m_sStyleName, StyleName);
+	CC_SYNTHESIZE_PASS_BY_REF(string, m_sFontName, FontName);
 	CC_SYNTHESIZE(int, m_nFontSize, FontSize);
 	CC_SYNTHESIZE(int, m_nLineSpace, LineSpace);
-	CC_SYNTHESIZE_IS_PASS_BY_REF(CAColor4B, m_cFontColor, FontColor);
+	CC_SYNTHESIZE_PASS_BY_REF(CAColor4B, m_cFontColor, FontColor);
 	CC_SYNTHESIZE_IS(bool, m_bBold, Bold);
 	CC_SYNTHESIZE_IS(bool, m_bItalics, Italics);
 	CC_SYNTHESIZE_IS(bool, m_bWordWrap, WordWrap);
+};
+
+class CC_DLL CALabelStyleCache
+{
+public:
+	void addStyle(const string& sStyleName, const string& sFontName, int nFontSize, int nLineSpace, const CAColor4B& cFontColor,
+		bool bBold, bool bItalics, bool bWordWrap);
+
+	const CALabelStyle* getStyle(const string& sStyleName);
+
+	static CALabelStyleCache * sharedStyleCache();
+
+	static void purgeSharedStyleCache();
+
+private:
+	CALabelStyleCache();
+	~CALabelStyleCache();
+
+	map<string, CALabelStyle*> m_StyleMap;
 };
 
 NS_CC_END
