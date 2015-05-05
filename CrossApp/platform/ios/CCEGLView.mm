105dd1ab7e2600ef9c9624558abd27ea7fd6bcae
lihui <195814193@qq.com>
2015-05-05 13:24:52 +0800

    no message

--- CrossApp/platform/ios/CCEGLView.mm
+++ CrossApp/platform/ios/CCEGLView.mm
@@ -67,17 +67,17 @@ void CCEGLView::setIMEKeyboardState(bool bOpen)
 void CCEGLView::setIMEKeyboardNumber()
 {
     EAGLView * view = [EAGLView sharedEGLView];
-    view.keyboardType =UIKeyboardTypeNumberPad;
+    view.textfield.keyboardType =UIKeyboardTypeNumberPad;
 }
 void CCEGLView::setIMEKeyboardDefault()
 {
     EAGLView * view = [EAGLView sharedEGLView];
-    view.keyboardType =UIKeyboardTypeDefault;
+    view.textfield.keyboardType =UIKeyboardTypeDefault;
 }
 void CCEGLView::setIMEKeyboardAlphabet()
 {
     EAGLView * view = [EAGLView sharedEGLView];
-    view.keyboardType =UIKeyboardTypeASCIICapable;
+    view.textfield.keyboardType =UIKeyboardTypeASCIICapable;
 }
 CCEGLView* CCEGLView::sharedOpenGLView()
 {
@@ -87,25 +87,25 @@ CCEGLView* CCEGLView::sharedOpenGLView()
 void CCEGLView::setIMEKeyboardReturnSend()
 {
     EAGLView * view = [EAGLView sharedEGLView];
-    view.returnKeyType =UIReturnKeySend;
+    view.textfield.returnKeyType =UIReturnKeySend;
 }
 
 void CCEGLView::setIMEKeyboardReturnSearch()
 {
     EAGLView * view = [EAGLView sharedEGLView];
-    view.returnKeyType =UIReturnKeySearch;
+    view.textfield.returnKeyType =UIReturnKeySearch;
 }
 
 void CCEGLView::setIMEKeyboardReturnDone()
 {
     EAGLView * view = [EAGLView sharedEGLView];
-    view.returnKeyType =UIReturnKeyDone;
+    view.textfield.returnKeyType =UIReturnKeyDone;
 }
 
 void CCEGLView::setIMEKeyboardReturnEnter()
 {
     EAGLView * view = [EAGLView sharedEGLView];
-    view.returnKeyType =UIReturnKeyDefault;
+    view.textfield.returnKeyType =UIReturnKeyDefault;
 }
 NS_CC_END
 
