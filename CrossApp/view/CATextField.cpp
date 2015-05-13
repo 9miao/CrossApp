e56b8b70af47b8786ec5fb07a687fdd4c35fcbce
栗元峰 <278688386@qq.com>
2015-05-13 16:19:19 +0800

    no message

--- CrossApp/view/CATextField.cpp
+++ CrossApp/view/CATextField.cpp
@@ -461,7 +461,8 @@ void CATextField::insertText(const char * text, int len)
     CC_RETURN_IF(len <= 0);
     CC_RETURN_IF(text == 0);
     
-    if (!strcmp(text, "\n")) {
+    if (!strcmp(text, "\n"))
+    {
         getKeyBoradReturnCallBack();
         return;
     }
