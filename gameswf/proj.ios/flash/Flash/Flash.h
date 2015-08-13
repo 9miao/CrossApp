f8999cae36063a1f7cfd732dd8639a0626587d57
qiaoxin <qiaoxin265@126.com>
2015-08-13 10:16:56 +0800

    Initial Commit

---
+++ gameswf/proj.ios/flash/Flash/Flash.h
@@ -0,0 +1,13 @@
+//
+//  Flash.h
+//  Flash
+//
+//  Created by Jerson on 15/8/13.
+//  Copyright (c) 2015年 qiaoxin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Flash : NSObject
+
+@end
---
+++ gameswf/proj.ios/flash/Flash/Flash.m
@@ -0,0 +1,13 @@
+//
+//  Flash.m
+//  Flash
+//
+//  Created by Jerson on 15/8/13.
+//  Copyright (c) 2015年 qiaoxin. All rights reserved.
+//
+
+#import "Flash.h"
+
+@implementation Flash
+
+@end
