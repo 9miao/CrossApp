843dfd66b96754a3db3138daa1f7ce4ad1ab52e5
栗元峰 <278688386@qq.com>
2015-11-01 21:04:44 +0800

    no message

--- CrossApp/the_third_party/FFmpeg/include/libavcodec/d3d11va.h
+++ CrossApp/the_third_party/FFmpeg/include/libavcodec/d3d11va.h
@@ -53,10 +53,8 @@
  * to the Direct3D11 FFmpeg HWAccel implementation.
  *
  * The application must make it available as AVCodecContext.hwaccel_context.
- *
- * Use av_d3d11va_alloc_context() exclusively to allocate an AVD3D11VAContext.
  */
-typedef struct AVD3D11VAContext {
+struct AVD3D11VAContext {
     /**
      * D3D11 decoder object
      */
@@ -91,19 +89,7 @@ typedef struct AVD3D11VAContext {
      * Private to the FFmpeg AVHWAccel implementation
      */
     unsigned report_id;
-
-    /**
-      * Mutex to access video_context
-      */
-    HANDLE  context_mutex;
-} AVD3D11VAContext;
-
-/**
- * Allocate an AVD3D11VAContext.
- *
- * @return Newly-allocated AVD3D11VAContext or NULL on failure.
- */
-AVD3D11VAContext *av_d3d11va_alloc_context(void);
+};
 
 /**
  * @}
